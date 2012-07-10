0d38a9e3bfa2568112791c2cfa06fc530f74ac12
mikefab <mikefabrikant@gmail.com>
2012-07-09 20:01:05 -0400

    level 6

---
+++ player.rb
@@ -0,0 +1,29 @@
+class Player
+  def play_turn(warrior)
+    @health ||= warrior.health
+    siege = (warrior.health<@health) ? true : false
+    if warrior.feel.empty?
+      if siege
+        if warrior.health < 10
+          warrior.walk!(:backward)
+        else
+          warrior.walk!
+        end
+      else
+        if warrior.health <= 19 
+          warrior.rest!
+        else 
+          warrior.walk!
+        end
+      end
+    else
+      if warrior.feel.captive?
+        warrior.rescue!
+      else
+        warrior.attack!
+      end
+    end
+    @health = warrior.health  
+    # add your code here
+  end
+end

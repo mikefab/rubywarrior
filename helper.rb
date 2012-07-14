687ebf902f553ec13e1172a98ee753ed216b62ef
mikefab <mikefabrikant@gmail.com>
2012-07-14 12:54:14 -0400

    typo correction

--- helper.rb
+++ helper.rb
@@ -1,9 +1,9 @@
 module Helper
 
-  ENEMIES = ["Wizard", "Archer", "Sludge"]
+  ENEMIES = ["Wizard","Archer","Sludge"]
 
   def Helper.fit?(warrior)
-    if warrior.health <=19 && warrior.health <= 19
+    if warrior.health <= 19
       return true
     else
       return false
@@ -16,12 +16,11 @@ module Helper
 
   def Helper.shoot?(warrior)
     warrior.look.size.times do |t|
-      [:forward,:backward].each do |dir|
+      [:forward,:backward,:left,:right].each do |dir|
         ENEMIES.each do |enemy|
           if warrior.look(dir)[t].to_s == "#{enemy}"
             flag = 1
             t.times do |t2|
-              print "#{t} #{t2+1} <--- t2\n"
               flag = 0 unless  warrior.look(dir)[t-(t2+1)].to_s == "nothing"
             end
             return dir if flag == 1
@@ -29,6 +28,6 @@ module Helper
         end
       end
     end
-  return nil
+   return nil
   end
 end
